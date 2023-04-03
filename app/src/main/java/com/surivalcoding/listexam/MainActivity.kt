3001f1061bbc726366517288f1e57f3722ce3ca3
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-04-03 13:15:36 +0900

    Initial commit

---
+++ app/src/main/java/com/surivalcoding/listexam/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.surivalcoding.listexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // data 준비
+        val items = (1..100).toList()
+
+        // 어댑터(data - view 연결) 준비
+        val adapter = CustomAdapter(
+            items.map { it.toString() }
+                .toTypedArray()
+        )
+
+        // View 준비
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // View에 Adapter 연결
+        recyclerView.adapter = adapter
+    }
+}
