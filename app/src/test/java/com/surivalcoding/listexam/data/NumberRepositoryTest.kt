f1db559837c3f11ee08758fc071d9bdee22e99ea
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-04-04 10:12:35 +0900

    add : Test 코드 작성

--- app/src/test/java/com/surivalcoding/listexam/data/NumberRepositoryTest.kt
+++ app/src/test/java/com/surivalcoding/listexam/data/NumberRepositoryTest.kt
@@ -4,24 +4,48 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class NumberRepositoryTest {
-
-    @Test
-    fun insert() {
-
-    }
+    private val repository = NumberRepository()
 
     @Test
     fun update() {
+        repository.update(
+            Number(
+                value = 1,
+                isSelected = true,
+            )
+        )
 
+        assertEquals(true, repository.findAll().first().isSelected)
+
+        repository.update(
+            Number(
+                value = 1,
+                isSelected = false,
+            )
+        )
+
+        assertEquals(false, repository.findAll().first().isSelected)
     }
 
     @Test
     fun delete() {
+        repository.delete(Number(
+            value = 1,
+            isSelected = true,
+        ))
 
+        assertEquals(99, repository.findAll().size)
+
+        repository.delete(Number(
+            value = 2,
+            isSelected = true,
+        ))
+
+        assertEquals(98, repository.findAll().size)
     }
 
     @Test
     fun `데이터를 잘 가져와야 한다`() {
-
+        assertEquals(100, repository.findAll().size)
     }
 }
